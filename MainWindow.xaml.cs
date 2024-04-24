9f7f6f50ae310d1487f1bc70d0d120c7aeb7dd14
LazyKote <kate.karakuts@gmail.com>
2024-04-24 21:43:59 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Windows;
+using Google.Protobuf.WellKnownTypes;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
+using Mysqlx.Cursor;
+using OfficeOpenXml;
+using System.IO;
+
+namespace AttendanceEmp
+{
+    public partial class MainWindow : Window
+    {
+        private string connectionString;
+
+        public MainWindow()
+        {
+            InitializeComponent(); // Инициализация XAML главного окна
+            connectionString = "Server=localhost;Database=empattend;Uid=root;Pwd=Kate+Kate19;"; // Установка соединения с бд
+        }   
+
+        // Method to load employee data from the database and populate the DataGrid
+        private void LoadEmployeeData()
+        {
+            string sql = "SELECT * FROM employees";// SQL query to select all columns from the employees table
+            DataTable employeeData = new DataTable();
+
+            // Establish a connection to the MySQL database
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))// Create a command to execute the SQL query
+                using (MySqlCommand command = new MySqlCommand(sql, connection))// Create a data adapter to fill the DataTable with the query results
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))// Execute the query and fill the DataTable
+                {
+                    connection.Open();
+                    adapter.Fill(employeeData);
+                }
+
+                // Bind the DataTable to the DataGrid
+                EmployeeDataGrid.ItemsSource = employeeData.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void DataRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            // This button click event can be used to refresh the employee data
+            LoadEmployeeData();
+        }
+        // Method to search for employees by position
+        private void SearchEmployeesByAttribute(string attributeName, string attributeValue)
+        {
+            // Construct the SQL query dynamically based on the attribute name and value
+            string sql = $"SELECT * FROM employees WHERE {attributeName} = @AttributeValue";
+            DataTable employeeData = new DataTable();
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                using (MySqlCommand command = new MySqlCommand(sql, connection))
+                {
+                    // Add parameter to prevent SQL injection
+                    command.Parameters.AddWithValue("@AttributeValue", attributeValue);
+
+                    connection.Open();
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {
+                        adapter.Fill(employeeData);
+                    }
+                }
+
+                EmployeeDataGrid.ItemsSource = employeeData.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+        private void DBSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the search term from the TextBox
+            string attributeValue = DBSearchBox.Text;
+            string attributeName = DBSearchBox.Text;
+            // Determine which RadioButton is selected and search by the corresponding attribute
+            if ((bool)PositionRadioButton.IsChecked)
+            {
+                SearchEmployeesByAttribute("Должность", attributeName);
+            }
+            else if ((bool)FullNameRadioButton.IsChecked)
+            {
+                SearchEmployeesByAttribute("ФИО", attributeName);
+            }
+        }
+        private void ExportToExcelButton_Click(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT * FROM employees";
+            DataTable empData = new DataTable();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            using (MySqlCommand command = new MySqlCommand(sql, connection))
+            using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+            {
+                connection.Open();
+                adapter.Fill(empData);
+            }
+
+            // Создание нового Excel-файла
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                // Добавление нового листа в Excel-файл
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Employees Data");
+
+                // Заполнение листа данными из DataTable
+                worksheet.Cells["A1"].LoadFromDataTable(empData, true);
+
+                // Сохранение Excel-файла
+                string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "EmployeesData.xlsx");
+                FileInfo excelFile = new FileInfo(filePath);
+                package.SaveAs(excelFile);
+            }
+
+            MessageBox.Show("Данные успешно экспортированы в Excel-файл.");
+        }
+
+        private void NewFormButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,182 @@
+﻿using MySql.Data.MySqlClient;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AttendanceEmp
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        double totalHoursWorked = 0;
+        private string connectionString;
+        public Window1()
+        {
+            InitializeComponent();
+            connectionString = "Server=localhost;Database=empattend;Uid=root;Pwd=Kate+Kate19;"; // Установка соединения с бд
+        }
+        private void LoadAttendenceData()
+        {
+            string sql = "SELECT * FROM attendance"; // SQL query to select all columns from the attendance table
+            DataTable attendData = new DataTable();
+
+            // Establish a connection to the MySQL database
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                using (MySqlCommand command = new MySqlCommand(sql, connection))
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                {
+                    connection.Open();
+                    adapter.Fill(attendData);
+                }
+
+                // Bind the DataTable to the DataGrid
+                AttendenceData.ItemsSource = attendData.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void AttendDataLoad_Click(object sender, RoutedEventArgs e)
+        {
+            // This button click event can be used to refresh the attendance data
+            LoadAttendenceData();
+        }
+
+        public double CalculateHoursWorkedForMonth(string employeeName, int month, int year)
+        {
+            double totalHoursWorked = 0;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+
+                command.CommandText = @"SELECT ВремяВхода, ВремяВыхода 
+                                    FROM attendance 
+                                    WHERE ФИО = @EmployeeName AND MONTH(ВремяВхода) = @Month AND YEAR(ВремяВхода) = @Year";
+
+                command.Parameters.AddWithValue("@EmployeeName", employeeName);
+                command.Parameters.AddWithValue("@Month", month);
+                command.Parameters.AddWithValue("@Year", year);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        DateTime arrivalTime = reader.GetDateTime(0);
+                        DateTime departureTime = reader.IsDBNull(1) ? DateTime.Now : reader.GetDateTime(1);
+
+                        TimeSpan timeWorked = departureTime - arrivalTime;
+                        totalHoursWorked += timeWorked.TotalHours;
+                    }
+                }
+            }
+
+            return totalHoursWorked;
+        }
+
+        private void CountButton_Click(object sender, RoutedEventArgs e)
+        {
+            int month = Convert.ToInt32(MonthText.Text);
+            int year = Convert.ToInt32(YearText.Text);
+            string employeeName = FullNameText.Text;
+
+            if (HoursRadioButton.IsChecked == true)
+            {
+                double totalHoursWorked = CalculateHoursWorkedForMonth(employeeName, month, year);
+                MessageBox.Show($"Количество часов работы за месяц: {Convert.ToString(totalHoursWorked)}");
+            }
+            else if(AbsenseRadioButton.IsChecked == true)
+            {
+                int daysWithoutAttendance = CalculateDaysWithoutAttendance(employeeName, month, year);
+                MessageBox.Show($"Количество дней без посещения: {daysWithoutAttendance}");
+            }
+        }
+        public int CalculateDaysWithoutAttendance(string employeeName, int month, int year)
+        {
+            int totalDaysInMonth = DateTime.DaysInMonth(year, month);
+            int totalAttendanceDays = 0;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            using (MySqlCommand command = new MySqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+
+                // Подсчет количества записей для сотрудника
+                command.CommandText = @"SELECT COUNT(*) 
+                                FROM attendance 
+                                WHERE ФИО = @EmployeeName AND MONTH(ВремяВхода) = @Month AND YEAR(ВремяВхода) = @Year";
+
+                command.Parameters.AddWithValue("@EmployeeName", employeeName);
+                command.Parameters.AddWithValue("@Month", month);
+                command.Parameters.AddWithValue("@Year", year);
+
+                totalAttendanceDays = Convert.ToInt32(command.ExecuteScalar());
+            }
+
+            // Вычитаем количество дней отсутствия от общего количества дней в месяце
+            int daysWithoutAttendance = totalDaysInMonth - totalAttendanceDays-8;
+
+            return daysWithoutAttendance;
+        }
+        private void ExportToExcelButton_Click(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT * FROM attendance";
+            DataTable ettendData = new DataTable();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            using (MySqlCommand command = new MySqlCommand(sql, connection))
+            using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+            {
+                connection.Open();
+                adapter.Fill(ettendData);
+            }
+            foreach (DataRow row in ettendData.Rows)
+            {
+                // Явно указываем тип данных для столбца ВремяВхода
+                ettendData.Columns["ВремяВхода"].DataType = typeof(DateTime);
+            }
+
+            // Создание нового Excel-файла
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                // Добавление нового листа в Excel-файл
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Attendance Data");
+
+                // Заполнение листа данными из DataTable
+                worksheet.Cells["A1"].LoadFromDataTable(ettendData, true);
+
+                // Сохранение Excel-файла
+                string filePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "AttendanceData.xlsx");
+                FileInfo excelFile = new FileInfo(filePath);
+                package.SaveAs(excelFile);
+            }
+
+            MessageBox.Show("Данные успешно экспортированы в Excel-файл.");
+        }
+    }
+}
+
+
